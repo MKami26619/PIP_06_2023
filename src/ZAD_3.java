91347334fc8deac5c8b1eacb1fca6a32fda9eac6
Mike <115941325+MKami26619@users.noreply.github.com>
2023-01-08 14:16:17 +0100

    Update ZAD_3.java

--- src/ZAD_3.java
+++ src/ZAD_3.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class ZAD_3 {
+public class ZAD_3 {
+    public static void main(String[] args) {
+        int[] zakres = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] reversedZakres;
+        reversedZakres = reverseZakres(zakres);
+        printZakres(reversedZakres);
+    }
+
+    public static int[] reverseZakres(int[] zakres) {
+        int[] reversedZakres = new int[zakres.length];
+
+        for (int i = 0; i < zakres.length; i++) {
+            reversedZakres[i] = zakres[zakres.length - 1 - i];
+        }
+
+        return reversedZakres;
+    }
+
+    public static void printZakres(int[] zakres) {
+        for (int element : zakres) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
 }
