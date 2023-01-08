60eb4f906707baa781edd190c42bc9af5ed54514
Mike <115941325+MKami26619@users.noreply.github.com>
2023-01-08 14:16:35 +0100

    Update ZAD_2.java

--- src/ZAD_2.java
+++ src/ZAD_2.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class ZAD_2 {
+public class ZAD_2 {
+    public static void main(String[] args) {
+        int[] zakres = {5, 3, 7, 1, 4, 2, 6, -10, 0, -5};
+        int[] sortedZakres = sortZakres(zakres);
+        printZakres(sortedZakres);
+    }
+
+    public static int[] sortZakres(int[] zakres) {
+        int[] sortedZakres = zakres.clone();
+
+        for (int i = 0; i < sortedZakres.length - 1; i++) {
+            for (int j = 0; j < sortedZakres.length - 1; j++) {
+                if (sortedZakres[j] > sortedZakres[j + 1]) {
+                    int temp = sortedZakres[j];
+                    sortedZakres[j] = sortedZakres[j + 1];
+                    sortedZakres[j + 1] = temp;
+                }
+            }
+        }
+
+        return sortedZakres;
+    }
+
+    public static void printZakres(int[] zakres) {
+        for (int element : zakres) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
 }
